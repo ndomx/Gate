b0693eba415c455f1a3d364dcfd1c9daea9b7218
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-11-06 22:33:48 -0300

    use consistent notation

--- embedded/rpi/python/main.py
+++ embedded/rpi/python/main.py
@@ -4,10 +4,10 @@ from state_machine import StateMachine
 RELAY_ON_DURATION = 1
 
 if __name__ == '__main__':
-    mqttClient = MqttClient()
+    mqtt_client = MqttClient()
     state_machine = StateMachine(RELAY_ON_DURATION)
 
-    mqttClient.connect('home/main', lambda msg: state_machine.set_flag())
+    mqtt_client.connect('home/main', lambda msg: state_machine.set_flag())
 
     while True:
         state_machine.run()
