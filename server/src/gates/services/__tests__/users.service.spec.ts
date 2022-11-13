49740f399a0541c3ad47ea626140fd93310df628
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-11-13 20:24:28 -0300

    update dependencies

--- server/src/gates/services/__tests__/users.service.spec.ts
+++ server/src/gates/services/__tests__/users.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from '../services/users.service';
+import { UsersService } from '../users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
