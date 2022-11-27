a75d699affb765fd532619ba8a166f68b9f838f3
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-11-27 11:58:45 -0300

    install passport dependencies and create module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GatesModule } from './gates/gates.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB_URI),
     GatesModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
