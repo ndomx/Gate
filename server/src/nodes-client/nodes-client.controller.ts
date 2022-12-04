f6b783b1824eb089c37f71c2f558f0e17ec911ae
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-12-04 12:51:51 -0300

    fix routes

--- server/src/nodes-client/nodes-client.controller.ts
+++ server/src/nodes-client/nodes-client.controller.ts
@@ -8,8 +8,10 @@ import {
   Delete,
   Param,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { JwtAdminAuthGuard } from 'src/auth/guards/jwt-admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { NodeDto } from 'src/nodes/dtos/node.dto';
 import { CreateNodeRequestDto } from './dtos/create-node-request.dto';
 import { GetNodesResponseDto } from './dtos/get-nodes-response.dto';
@@ -23,26 +25,33 @@ export class NodesClientController {
   constructor(private readonly nodesClientService: NodesClientService) {}
 
   @Post()
+  @UseGuards(JwtAdminAuthGuard)
   create(@Body() request: CreateNodeRequestDto): Promise<NodeDto> {
     return this.nodesClientService.createNode(request);
   }
 
-  @Get(':nodeId')
+  @Get('/children/:nodeId')
+  @UseGuards(JwtAdminAuthGuard)
   async getChildren(
     @Param('nodeId') nodeId: string,
   ): Promise<GetNodesResponseDto> {
     return await this.nodesClientService.getChildren(nodeId);
   }
 
-  @Get('user/:userId')
+  @Get('/user')
+  @UseGuards(JwtAuthGuard)
   async getUserNodes(
-    @Param('userId') userId: string,
+    @Request() req,
     @Query('device_only') deviceOnly?: boolean,
   ): Promise<UserNodesResponseDto> {
-    return await this.nodesClientService.getUserNodes(userId, deviceOnly);
+    return await this.nodesClientService.getUserNodes(
+      req.user.userId,
+      deviceOnly,
+    );
   }
 
-  @Get()
+  @Get('/match')
+  @UseGuards(JwtAdminAuthGuard)
   async getMatching(
     @Query('prefix') prefix: string,
   ): Promise<GetNodesResponseDto> {
@@ -50,6 +59,7 @@ export class NodesClientController {
   }
 
   @Patch(':nodeId')
+  @UseGuards(JwtAdminAuthGuard)
   update(
     @Param('nodeId') nodeId: string,
     @Body() fields: UpdateNodeRequestDto,
@@ -58,6 +68,7 @@ export class NodesClientController {
   }
 
   @Delete(':nodeId')
+  @UseGuards(JwtAdminAuthGuard)
   remove(@Param('nodeId') nodeId: string): Promise<NodeDto> {
     return this.nodesClientService.deleteNode(nodeId);
   }
