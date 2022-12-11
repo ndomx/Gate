461ec91967940e5efde93f3495336695b3b395dd
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2022-12-10 22:43:50 -0300

    hide keyboard when button is pressed

--- clients/android/app/src/main/java/com/ndomx/gate/RegisterActivity.kt
+++ clients/android/app/src/main/java/com/ndomx/gate/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.ndomx.gate
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -38,6 +40,11 @@ class RegisterActivity : AppCompatActivity(R.layout.activity_register) {
     }
 
     private fun registerUser(username: String, password: String, host: String) {
+        currentFocus?.let { view ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
         PrefsManager.save(this, PrefsManager.HOST_URL_KEY, host)
         val request = RegisterRequestBody(
             username, password
