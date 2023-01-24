ba0e7da8281549f9f44cd5c9619e43ad8f6be473
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-01-23 22:54:59 -0300

    sample program

--- embedded/esp8266/ino/main/main.ino
+++ embedded/esp8266/ino/main/main.ino
@@ -1,9 +1,20 @@
+#include <wifi.h>
+#include <state_machine.h>
+
+#include "global_defs.h"
+
 void setup()
 {
+    StateMachine state_machine(OUTPUT_PIN);
 
+    Serial.begin(115200);
+    Serial.println("Connecting to WiFi");
+
+    wifi::connect_blocking();
+
+    Serial.println("Connected");
 }
 
 void loop()
 {
-    
 }
