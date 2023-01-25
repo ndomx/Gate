54d6dd6f2f5fa837010dc43c0791ce2b0b2c0b41
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-01-24 22:56:52 -0300

    update credentials

--- embedded/esp8266/ino/lib/mqtt/credentials.example.h
+++ embedded/esp8266/ino/lib/mqtt/credentials.example.h
@@ -3,6 +3,11 @@
 
 static const char* mqtt_broker_url = "...";
 static const int mqtt_broker_port = 1883;
+
+// Leave NULL if not used
+static const char* mqtt_username = NULL;
+static const char* mqtt_password = NULL;
+
 static const char* mqtt_topic = "...";
 
 #endif // LIB_MQTT_CREDENTIALS_H
