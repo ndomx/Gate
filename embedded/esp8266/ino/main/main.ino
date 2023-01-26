794df6d9cf7ddd789c08231102f106aeb11a0f76
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-01-26 20:59:43 -0300

    sample program

--- embedded/esp8266/ino/main/main.ino
+++ embedded/esp8266/ino/main/main.ino
@@ -1,20 +1,47 @@
-#include <wifi.h>
+#include <mqtt.h>
 #include <state_machine.h>
+#include <wifi.h>
 
 #include "global_defs.h"
 
+StateMachine state_machine(OUTPUT_PIN);
+
+void setup_error(const char* message)
+{
+    Serial.println(message);
+    while (true)
+    {
+    }
+}
+
 void setup()
 {
-    StateMachine state_machine(OUTPUT_PIN);
-
+    // debug serial
     Serial.begin(115200);
-    Serial.println("Connecting to WiFi");
+    while (!Serial)
+    {
+    }
 
     wifi::connect_blocking();
+    state_machine.init_gpio();
 
-    Serial.println("Connected");
+    bool success = mqtt::init([]() { state_machine.set_flag(); });
+    if (!success)
+    {
+        setup_error("Error initializing mqtt, please try again");
+    }
+
+    success = mqtt::connect();
+    if (!success)
+    {
+        setup_error("Error connecting to mqtt server");
+    }
+
+    Serial.println("Setup success");
 }
 
 void loop()
 {
+    mqtt::run();
+    state_machine.run();
 }
