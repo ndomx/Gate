1107186ac2b5271a96b551a625da9f3bb6bc5cec
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-02-11 20:28:44 -0300

    add more constructors

--- embedded/esp8266/ino/lib/io/io_state_machine.h
+++ embedded/esp8266/ino/lib/io/io_state_machine.h
@@ -5,17 +5,32 @@
 
 #include "io_states.h"
 
-#define DEFAULT_TIMEOUT_MS 1000
+#define DEFAULT_TIMEOUT_MS (1000U)
+#define DEFAULT_INVERTED_LOGIC (false)
 
 class IO_StateMachine
 {
     public:
     IO_StateMachine(
-        const uint8_t output_pin, 
-        const uint32_t active_timeout_ms = DEFAULT_TIMEOUT_MS,
-        const uint32_t disabled_timeout_ms = DEFAULT_TIMEOUT_MS,
-        const bool inverted_logic = false
-    ) : _output_pin(output_pin), _active_timeout(active_timeout_ms), _disabled_timeout(disabled_timeout_ms), _inverted_logic(inverted_logic) {} 
+        const uint8_t output_pin, const bool inverted_logic, const uint32_t active_timeout_ms,
+        const uint32_t disabled_timeout_ms
+    )
+        : _output_pin(output_pin), _inverted_logic(inverted_logic), _active_timeout(active_timeout_ms),
+          _disabled_timeout(disabled_timeout_ms)
+    {
+    }
+
+    IO_StateMachine(const uint8_t output_pin, const bool invert_logic)
+        : _output_pin(output_pin), _inverted_logic(invert_logic), _active_timeout(DEFAULT_TIMEOUT_MS),
+          _disabled_timeout(DEFAULT_TIMEOUT_MS)
+    {
+    }
+
+    IO_StateMachine(const uint8_t output_pin)
+        : _output_pin(output_pin), _inverted_logic(DEFAULT_INVERTED_LOGIC), _active_timeout(DEFAULT_TIMEOUT_MS),
+          _disabled_timeout(DEFAULT_TIMEOUT_MS)
+    {
+    }
 
     void init(void);
     void set_flag(void);
