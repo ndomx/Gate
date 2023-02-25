6baca2977e524c0489aac462af01bf5c64dd041a
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-02-25 15:13:49 -0300

    receive node display name

--- clients/flutter_client/lib/src/http/dtos/common/node_dto.dart
+++ clients/flutter_client/lib/src/http/dtos/common/node_dto.dart
@@ -5,14 +5,17 @@ class NodeDto {
   final String rootId;
   final String parentId;
   final String name;
+  final String displayName;
   final NodeInfoDto nodeInfo;
 
-  const NodeDto(
-      {required this.id,
-      required this.rootId,
-      required this.parentId,
-      required this.name,
-      required this.nodeInfo});
+  const NodeDto({
+    required this.id,
+    required this.rootId,
+    required this.parentId,
+    required this.name,
+    required this.displayName,
+    required this.nodeInfo,
+  });
 
   factory NodeDto.fromJson(Map<String, dynamic> json) {
     return NodeDto(
@@ -20,6 +23,7 @@ class NodeDto {
         rootId: json['rootId'],
         parentId: json['parent'],
         name: json['name'],
+        displayName: json['displayName'],
         nodeInfo: NodeInfoDto.fromJson(json['nodeInfo']));
   }
 
@@ -29,6 +33,7 @@ class NodeDto {
       'rootId': rootId,
       'parent': parentId,
       'name': name,
+      'displayName': displayName,
       'nodeInfo': nodeInfo.toJson(),
     };
   }
