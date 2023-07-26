5d8057226033181402762c002d67e82c88c4e957
ndomx-mig <ndominguez@migrante.com>
2023-07-25 20:54:30 -0400

    feat(server): throw if query is not successful

--- server/src/nodes/services/nodes-crud.service.ts
+++ server/src/nodes/services/nodes-crud.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Node, NodeDocument } from '../schemas/node.schema';
 import { NodeResponseDto } from '../dtos/responses';
 import { plainToInstance } from 'class-transformer';
 import { CreateNodeRequestDto, UpdateNodeRequestDto } from '../dtos/requests';
+import { ErrorCodes } from 'src/common/enum/error-codes.enum';
 
 @Injectable()
 export class NodesCrudService {
@@ -14,16 +19,37 @@ export class NodesCrudService {
 
   async create(node: CreateNodeRequestDto): Promise<NodeResponseDto> {
     const created = await this.nodeModel.create(node);
+    if (!created) {
+      throw new InternalServerErrorException({
+        errorCode: ErrorCodes.DATABASE_ERROR,
+        message: 'could not create resource',
+      });
+    }
+
     return this.#mapFromSchema(created);
   }
 
   async findById(nodeId: string): Promise<NodeResponseDto> {
     const node = await this.nodeModel.findById(nodeId);
+    if (!node) {
+      throw new NotFoundException({
+        errorCode: ErrorCodes.NODE_NOT_FOUND,
+        message: 'could not find node',
+      });
+    }
+
     return this.#mapFromSchema(node);
   }
 
   async findInRoot(nodeId: string, rootId: string): Promise<NodeResponseDto> {
     const node = await this.nodeModel.findOne({ _id: nodeId, rootId });
+    if (!node) {
+      throw new NotFoundException({
+        errorCode: ErrorCodes.NODE_NOT_FOUND,
+        message: 'could not find node',
+      });
+    }
+
     return this.#mapFromSchema(node);
   }
 
@@ -32,29 +58,67 @@ export class NodesCrudService {
     name: string,
   ): Promise<NodeResponseDto> {
     const node = await this.nodeModel.findOne({ parent: parentId, name });
+    if (!node) {
+      throw new NotFoundException({
+        errorCode: ErrorCodes.NODE_NOT_FOUND,
+        message: 'could not find node',
+      });
+    }
+
     return this.#mapFromSchema(node);
   }
 
   async findChildren(nodeId: string): Promise<NodeResponseDto[]> {
     const nodes = await this.nodeModel.find({ parent: nodeId });
+    if (!nodes) {
+      throw new InternalServerErrorException({
+        errorCode: ErrorCodes.NODE_NOT_FOUND,
+        message: 'resource not found',
+      });
+    }
+
     return nodes.map((node) => this.#mapFromSchema(node));
   }
 
   async findRoot(rootId: string): Promise<NodeResponseDto> {
     const root = await this.nodeModel.findOne({ _id: rootId, parent: '' });
+    if (!root) {
+      throw new NotFoundException({
+        errorCode: ErrorCodes.ROOT_NOT_FOUND,
+        message: 'could not find root',
+      });
+    }
+
     return this.#mapFromSchema(root);
   }
 
   async update(
     nodeId: string,
-    updateFields: UpdateNodeRequestDto,
+    fields: UpdateNodeRequestDto,
   ): Promise<NodeResponseDto> {
-    const node = await this.nodeModel.findByIdAndUpdate(nodeId, updateFields);
+    const node = await this.nodeModel.findByIdAndUpdate(nodeId, fields, {
+      returnDocument: 'after',
+    });
+
+    if (!node) {
+      throw new InternalServerErrorException({
+        errorCode: ErrorCodes.DATABASE_ERROR,
+        message: 'could not modify resource',
+      });
+    }
+
     return this.#mapFromSchema(node);
   }
 
   async delete(nodeId: string): Promise<NodeResponseDto> {
     const node = await this.nodeModel.findByIdAndDelete(nodeId);
+    if (!node) {
+      throw new NotFoundException({
+        errorCode: ErrorCodes.NODE_NOT_FOUND,
+        message: 'could not find node',
+      });
+    }
+
     return this.#mapFromSchema(node);
   }
 
