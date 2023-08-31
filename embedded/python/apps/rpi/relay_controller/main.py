75285da845129b9958c9a0aa48c950999ce76010
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-08-30 20:54:03 -0400

    refactor(embd/python): add pin table file

--- embedded/python/apps/rpi/relay_controller/main.py
+++ embedded/python/apps/rpi/relay_controller/main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from apps.rpi.libs.common.pin_table import load_bcm
 from state_machine import StateMachine
 from libs.mqtt.mqtt_client import MqttClient
 import libs.config as config
@@ -7,13 +8,16 @@ import libs.config as config
 from gpiozero import DigitalOutputDevice as digital_output
 from gpiozero import Button as button
 
-config_file = Path() / 'secrets.json'
+config_file = Path() / 'config.json'
 
 if __name__ == '__main__':
     config.load_secrets(config_file)
 
-    output_controller = digital_output(os.environ.get('RELAY_CONTROLLER_PIN'))
-    input_controller = button(os.environ.get('BYPASS_BUTTON_PIN'), pull_up=True)
+    output_pin = load_bcm('RELAY_CONTROLLER_PIN')
+    input_pin = load_bcm('BYPASS_BUTTON_PIN')
+
+    output_controller = digital_output(output_pin, initial_value=False)
+    input_controller = button(input_pin, pull_up=True)
 
     state_machine = StateMachine(output_controller)
     input_controller.when_pressed = state_machine.set_flag
