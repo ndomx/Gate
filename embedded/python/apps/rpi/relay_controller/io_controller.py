9482fbb320b3a2ee1c2e54005c22775aa206acd9
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2023-09-23 18:46:31 -0300

    feat(embd/python/rpi): add io-controller

---
+++ embedded/python/apps/rpi/relay_controller/io_controller.py
@@ -0,0 +1,28 @@
+from libs.common.command import Command
+from libs.io.digital_io_controller import DigitalIOController
+
+from gpiozero import DigitalOutputDevice as digital_output
+from gpiozero import Button as button
+
+
+class IOController(DigitalIOController):
+    def __init__(self, output_pin: int, initial_state: bool):
+        super.__init__(initial_state)
+
+        self.output = digital_output(output_pin, initial_value=initial_state)
+
+    def on(self):
+        self.output.on()
+        self.__update_state()
+    
+    def off(self):
+        self.output.off()
+        self.__update_state()
+    
+    def toggle(self):
+        self.output.toggle()
+        self.__update_state()
+    
+    def __update_state(self):
+        self.state = self.output.is_active
+    
