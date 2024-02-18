16c20ef0d9b981407e0069e96cc8c5e2f9e92402
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-02-18 19:44:17 -0300

    add react-icons

--- clients/react-client/src/components/NodeCard.tsx
+++ clients/react-client/src/components/NodeCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { GateNode } from "../utils/types";
 import { startStatusPolling } from "../utils/gate-client";
+import { IoIosArrowForward } from "react-icons/io";
 
 type NodeCardProps = {
   node: GateNode;
@@ -43,20 +44,7 @@ export default function NodeCard({ node, onClick }: NodeCardProps) {
         <p className="text-gray-500">{node.actionCode}</p>
       </div>
       <div className="w-10 h-10 flex items-center justify-center bg-blue-500 text-white rounded-full">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 5l7 7-7 7"
-          />
-        </svg>
+        <IoIosArrowForward size={24} />
       </div>
     </div>
   );
