015d6647b53223273dcb84bfbdbca1b5922897fa
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-02-19 16:59:22 -0300

    implement logout button

--- clients/react-client/src/components/TopBanner.tsx
+++ clients/react-client/src/components/TopBanner.tsx
@@ -11,7 +11,10 @@ export default function TopBanner({ userName, sideButton }: TopBannerProps) {
     <div className="bg-white text-black p-4 flex justify-end items-center">
       <div>
         <span className="mr-4">Welcome, {userName}</span>
-        <button className="px-4 py-2 bg-red-600 hover:bg-red-700 rounded-md text-white font-semibold">
+        <button
+          className="px-4 py-2 bg-red-600 hover:bg-red-700 rounded-md text-white font-semibold"
+          onClick={sideButton.onClick}
+        >
           {sideButton.label}
         </button>
       </div>
