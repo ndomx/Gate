2d680ea47b46563a792693401af9da7bede27738
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-02-18 22:39:10 -0300

    add spinner

--- clients/react-client/src/components/AccessIcon.tsx
+++ clients/react-client/src/components/AccessIcon.tsx
@@ -1,6 +1,7 @@
 import { IoIosArrowForward } from "react-icons/io";
 import { MdLock, MdLockOpen } from "react-icons/md";
 import { NodeStatus } from "../utils/types";
+import { ClipLoader } from "react-spinners";
 
 type AccessIconProps = {
   status: NodeStatus;
@@ -16,8 +17,20 @@ export default function AccessIcon({ status }: AccessIconProps) {
       );
     case "loading":
       return (
-        <div className="w-10 h-10 flex items-center justify-center bg-purple-500 text-white rounded-full">
-          <IoIosArrowForward size={24} />
+        <div className="w-10 h-10 flex items-center justify-center bg-purple-500 text-white rounded-full relative">
+          <div className="absolute inset-2">
+            <IoIosArrowForward size={24} />
+          </div>
+          <div className="absolute inset-0">
+            <ClipLoader
+              color="#3b82f6"
+              loading={true}
+              // cssOverride={override}
+              size={40}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
         </div>
       );
     case "access-rejected":
