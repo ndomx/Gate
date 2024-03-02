b1e8a627d9c438ff225f64980d60ecdbcc44c03b
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-03-02 17:40:48 -0300

    feat: top banner adjustments

--- clients/nextjs-client/src/app/gates/page.tsx
+++ clients/nextjs-client/src/app/gates/page.tsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { toast } from "react-toastify";
 import { auth } from "@clerk/nextjs";
 import { getUserNodes } from "@/utils/gate-client";
 import Nodes from "../components/nodes";
+import TopBanner from "../components/top-banner";
 
 export default async function Gates() {
   const { userId } = auth();
   const { nodes } = await getUserNodes(userId || "");
 
-  const nodeOnClick = async (nodeId: string) => {
-    toast.info("Activating device");
-    // await activateNode(user.id, nodeId);
-  };
-
   return (
     <div className="max-w-[600px] mx-auto my-1 p-4 container">
-      <h1 className="text-3xl font-bold mb-4">Device List</h1>
+      <TopBanner title="Device List" />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Nodes nodes={nodes || []} />
       </div>
