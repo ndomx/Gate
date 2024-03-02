3808f84f24e3b159128d383b67da413d5ff99281
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-03-02 17:30:01 -0300

    feat: node list

--- clients/nextjs-client/src/app/components/nodes.tsx
+++ clients/nextjs-client/src/app/components/nodes.tsx
@@ -1,23 +1,25 @@
+"use client";
+
 import React from "react";
 import { toast } from "react-toastify";
 import { GateNode } from "@/utils/types";
 import NodeCard from "./node-card";
-import { auth } from "@clerk/nextjs";
 
-export default function Nodes() {
+type NodesProps = {
+  nodes: GateNode[];
+};
+
+export default function Nodes({ nodes }: NodesProps) {
   const nodeOnClick = async (nodeId: string) => {
     toast.info("Activating device");
     // await activateNode(user.id, nodeId);
   };
 
   return (
-    <div className="max-w-[600px] mx-auto my-1 p-4 container">
-      <h1 className="text-3xl font-bold mb-4">Device List</h1>
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {nodes?.map((node, index) => (
-          <NodeCard key={index} node={node} onClick={nodeOnClick} />
-        ))}
-      </div> */}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {nodes.map((node, index) => (
+        <NodeCard key={index} node={node} onClick={nodeOnClick} />
+      ))}
     </div>
   );
 }
