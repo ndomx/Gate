0b9633e16da0c4303fd3507f0fa6ac95fbfa120a
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-03-03 21:04:04 -0300

    activate nodes

--- clients/nextjs-client/src/app/components/nodes.tsx
+++ clients/nextjs-client/src/app/components/nodes.tsx
@@ -4,15 +4,17 @@ import React from "react";
 import { toast } from "react-toastify";
 import { GateNode } from "@/utils/types";
 import NodeCard from "./node-card";
+import axios from "axios";
 
 type NodesProps = {
+  userId: string;
   nodes: GateNode[];
 };
 
-export default function Nodes({ nodes }: NodesProps) {
+export default function Nodes({ nodes, userId }: NodesProps) {
   const nodeOnClick = async (nodeId: string) => {
     toast.info("Activating device");
-    // await activateNode(user.id, nodeId);
+    await axios.post("/api/nodes/activate", { userId, nodeId });
   };
 
   return (
--- clients/nextjs-client/src/app/gates/page.tsx
+++ clients/nextjs-client/src/app/gates/page.tsx
@@ -7,14 +7,14 @@ import { ToastContainer } from "react-toastify";
 
 export default async function Gates() {
   const { userId } = auth();
-  const { nodes } = await getUserNodes(userId || "");
+  const { nodes, user } = await getUserNodes(userId || "");
 
   return (
     <>
       <div className="max-w-[600px] mx-auto my-1 p-4 container">
         <TopBanner title="Device List" />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <Nodes nodes={nodes || []} />
+          <Nodes nodes={nodes || []} userId={user.id} />
         </div>
       </div>
       <ToastContainer autoClose={2000} position="bottom-center" />
