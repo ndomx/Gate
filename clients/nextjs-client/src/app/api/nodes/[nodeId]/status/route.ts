b17af5ff19058c46ac35bec2c0ea4b955598f3a4
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2024-03-16 13:03:30 -0300

    chore: minor changes (#51)
    * chore(clients/nextjs): remove logs
    
    * chore(clients): add params to signup page
    
    * chore(server): user external id
    
    * add node prefix to topic

--- clients/nextjs-client/src/app/api/nodes/[nodeId]/status/route.ts
+++ clients/nextjs-client/src/app/api/nodes/[nodeId]/status/route.ts
@@ -22,8 +22,6 @@ async function getCommandStatus(
   const path = `/gates/${nodeId}/status`;
   const res = await get(path);
 
-  console.log("getCommandStatus:", res.status);
-
   if (res.status != 200) {
     throwHttpError("GET", path, res.status);
   }
@@ -44,8 +42,6 @@ function startStatusPolling(nodeId: string, delay: number): Promise<number> {
       if (res.pending) {
         return;
       }
-
-      console.log(res)
 
       clearInterval(intervalId);
       resolve(res.responseCode ?? COMMAND_STATUS_RESPONSE_CODE.UNKNOWN_ERROR);
