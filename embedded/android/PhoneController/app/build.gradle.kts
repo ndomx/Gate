004da7e481e47dc8abd7389ee5c45991f56e22d1
Nicolas Dominguez <37314364+ndomx@users.noreply.github.com>
2025-02-19 00:49:46 -0300

    add mqtt dependencies

--- embedded/android/PhoneController/app/build.gradle.kts
+++ embedded/android/PhoneController/app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // MQTT
+    implementation(libs.org.eclipse.paho.client.mqttv3)
+    implementation(libs.org.eclipse.paho.android.service)
 }
